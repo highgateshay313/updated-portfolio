9899454ebb5db82e3125a91de728d8da88c8c183
Shay Highgate <highgateshay@yahoo.com>
2026-06-23 20:23:10 -0500

    still working on mobile ui

--- src/components/MyImage.jsx
+++ src/components/MyImage.jsx
@@ -3,7 +3,7 @@ import styles from "./myImage.module.css";
 export default function MyImage({ className }) {
   return (
     <section className={`${styles.myImage_container} ${className || ""}`}>
-      <div className={styles.myImage_div}></div>
+      <div className={styles.myImage_div}>test</div>
     </section>
   );
 }

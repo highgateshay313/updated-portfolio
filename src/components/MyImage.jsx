3c2be5fd34fa7291871cbc69640fff0873f2d59d
Shay Highgate <highgateshay@yahoo.com>
2026-06-26 13:54:11 -0500

    fixed mobile

--- src/components/MyImage.jsx
+++ src/components/MyImage.jsx
@@ -4,7 +4,7 @@ export default function MyImage({ className }) {
   return (
     <div className={`${styles.myImage_container} ${className || ""}`}>
       <section className={styles.myImage_section}>
-        <div className={styles.myImage_div}>test</div>
+        <div className={styles.myImage_div}></div>
       </section>
     </div>
   );

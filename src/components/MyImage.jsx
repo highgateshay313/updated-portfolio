1026e7fddafd16dd7cc65c14470112faadc2dcb7
Shay Highgate <highgateshay@yahoo.com>
2026-06-26 14:59:05 -0500

    Fixed myImage div

--- src/components/MyImage.jsx
+++ src/components/MyImage.jsx
@@ -1,10 +1,14 @@
 import styles from "./myImage.module.css";
+import ProfileImage from "../assets/portfolio-cleanup-2.png";
 
 export default function MyImage({ className }) {
+  console.log(styles);
   return (
     <div className={`${styles.myImage_container} ${className || ""}`}>
       <section className={styles.myImage_section}>
-        <div className={styles.myImage_div}></div>
+        <div
+          className={styles.myImage_div}
+          style={{ backgroundImage: `url(${ProfileImage})` }}></div>
       </section>
     </div>
   );
